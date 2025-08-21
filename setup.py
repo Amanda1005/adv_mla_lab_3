3380031b64da96ebd6667f40bbc59c44dc7b2a99
Amanda1005 <a19881005a@gmail.com>
2025-08-21 14:34:02 +1000

    Initial project setup with dataset and dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Advanced Machine Learning Lab 3: EDA and Baseline Model',
+    author='Nian-Ya, Weng',
+    license='MIT',
+)
